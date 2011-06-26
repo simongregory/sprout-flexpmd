f55988285f8a927e6a43c47bce5c1062ffd720a9
Simon Gregory <simon@helvector.org>
2011-06-26 20:34:27 +0100

    Supress stderr so we don't stop subsequent rake tasks.

--- lib/flexpmd/base.rb
+++ lib/flexpmd/base.rb
@@ -1,7 +1,37 @@
 module Sprout::System
 
   ##
-  # Monkey patch the base system so we can inject 'java -jar' etc..
+  # Monkey patch the base system so we control the output to stderr.
+  #
+  class BaseSystem
+
+    ##
+    # Creates a new process, executes the command
+    # and returns whatever the process wrote to stdout, or stderr.
+    #
+    def execute(tool, options='')
+      Sprout.stdout.puts("#{tool} #{options}")
+      runner = get_and_execute_process_runner(tool, options)
+      error  = runner.read_err
+      result = runner.read
+
+      if(result.size > 0)
+        Sprout.stdout.puts result
+      end
+
+      if(error.size > 0)
+        # We expect the process to write to stderror so simply print it, we don't want Sprouts 
+        # to stop executing.
+        Sprout.stdout.puts error
+      end
+
+      result || error
+    end
+    
+  end
+  
+  ##
+  # Monkey patch the unix system so we can run tool with 'java -jar'.
   #
   class UnixSystem < BaseSystem
 
@@ -19,6 +49,7 @@ module Sprout::System
       runner.execute_open4 "java -Xms64m -Xmx768m -jar "+ clean_path(tool), options
       runner
     end
-    
+     
   end
+  
 end
