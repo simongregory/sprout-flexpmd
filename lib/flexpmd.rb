a30cadfb43c1efb11bdfc8b57ef4d1e4ad1b588a
Simon Gregory <simon@helvector.org>
2011-06-28 08:28:03 +0100

    Adding the JAR_VERSION number to the download url, as it's very likely to be consitent.

--- lib/flexpmd.rb
+++ lib/flexpmd.rb
@@ -10,7 +10,7 @@ module FlexPMD
   JAR_VERSION = '1.2'
   VERSION = "#{JAR_VERSION}.2"
   MD5 = 'efd3fa459788fa3d32837628dc82e264'
-  ZIP = 'http://opensource.adobe.com/svn/opensource/flexpmd/maven-repository/release/com/adobe/ac/flex-pmd/1.2/flex-pmd-all-in-one-bundle-1.2.zip'
+  ZIP = "http://opensource.adobe.com/svn/opensource/flexpmd/maven-repository/release/com/adobe/ac/flex-pmd/#{JAR_VERSION}/flex-pmd-all-in-one-bundle-#{JAR_VERSION}.zip"
 end
 
 Sprout::Specification.new do |s|
